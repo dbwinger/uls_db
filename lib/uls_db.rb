c437bcb5d8e65f1c0a76f2428940913687e94e8a
dbwinger <dbwinger@gmail.com>
2019-09-25 16:36:24 -0400

    Add db setup

--- lib/uls_db.rb
+++ lib/uls_db.rb
@@ -1,14 +1,15 @@
-require "uls_db/version"
+require File.expand_path("./uls_db/version", __dir__)
+require 'pg'
 
 module UlsDb
   ROOT_DIR = File.expand_path('../', __dir__)
 
   def self.database_table_file_path database, table
-    File.expand_path("~/Downloads/TEMP/uls_db/#{database}/#{table}.dat")
+    File.expand_path("../uls_db_input/#{database}/#{table}.dat", ROOT_DIR)
   end
 
   def self.output_file_path name
-    File.expand_path("~/Downloads/TEMP/uls_db/output/#{name}.tsv")
+    File.expand_path("../uls_db_output/#{name}.tsv", ROOT_DIR)
   end
 
   def self.process_table database, table
@@ -25,6 +26,34 @@ module UlsDb
 
       fields = row[0..-1].split('|')
       yield fields
+    end
+  end
+
+  def self.setup_db
+    conn = PG::Connection.open
+    conn.exec_params("DROP DATABASE IF EXISTS fcc_uls")
+    conn.exec_params("CREATE DATABASE fcc_uls")
+    conn.close
+    conn = PG::Connection.open(dbname: :fcc_uls)
+    conn.exec_params(File.read File.expand_path('../db/schema.sql', __dir__))
+  end
+
+  def self.import_table_to_db database, table, clear = true
+    filename = UlsDb.database_table_file_path(database, table)
+    lines = %x{wc -l #{filename}}.split.first.to_i
+
+    conn = PG::Connection.open(dbname: :fcc_uls)
+    conn.exec_params "DELETE FROM uls_#{table}" if clear
+
+
+    File.readlines(filename).each_with_index do |row, i|
+      # Increments of 1k
+      if i / 1000 == i / 1000.0
+        print "\rProcessed #{i / 1000}k of #{lines / 1000}k rows"
+      end
+
+      values = row[0..-1].split('|').map { |v| "'#{v}'" }.join(',')
+      res = conn.exec_params("INSERT INTO uls_#{table} VALUES (#{values})")
     end
   end
 end
